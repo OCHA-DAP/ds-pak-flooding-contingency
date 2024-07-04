745df62b890a27cd2dd96a191a03de2e0a235ef7
Zack <zackarno@gmail.com>
2024-07-04 11:05:45 -0400

    adjust input basin zip file input path from local to blob for parquet creation

--- data-raw/01_upload_hydrobasins_asia.R
+++ data-raw/01_upload_hydrobasins_asia.R
@@ -5,14 +5,31 @@ library(janitor)
 library(tidyverse)
 library(arrow)
 library(geoarrow)
+source("R/utils.R")
 
 
 # I guess i could put the raw zip on the blob and download it as a tempfile,
 # but seems a bit annoying since im just running this once. Adding it as a **TODO**
 
-zf <- "/Users/zackarno/Downloads/hybas_as_lev01-12_v1c.zip"
-zf_vp <- paste0("/vsizip/",zf)
+es <- azure_endpoint_url()
+# storage endpoint
+se <- AzureStor::storage_endpoint(es, sas = Sys.getenv("DSCI_AZ_SAS_DEV"))
+# storage container
+sc_global <- AzureStor::storage_container(se, "global")
 
+
+# Load thresholds data.frame ----------------------------------------------
+
+
+tf <- tempfile(fileext = ".zip")
+
+AzureStor$download_blob(
+  container = sc_global,
+  src = "vector/hybas_as_lev01-12_v1c.zip",
+  dest = tf
+)
+
+zf_vp <- paste0("/vsizip/",tf)
 st_layers(zf_vp)
 
 gdf_basins <- st_layers(zf_vp)$name |> 
